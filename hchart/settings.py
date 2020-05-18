25a0c21bd7f367298e595786ca0cb785bf53510f
ariesunique <ariesunique@gmail.com>
2020-05-18 01:03:22 -0400

    Remove unnecessary var from env file; put in settings instead

--- hchart/settings.py
+++ hchart/settings.py
@@ -6,4 +6,4 @@ env.read_env()
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
 SECRET_KEY = env.str("SECRET_KEY")
-MAX_CONTENT_LENGTH = env.int("MAX_CONTENT_LENGTH")
+MAX_CONTENT_LENGTH = 204800 # max allowed file size is 200 kb (200 * 1024)
